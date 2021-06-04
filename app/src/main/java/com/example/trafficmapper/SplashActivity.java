47280211d09d9c57dc2239c38016fe9bc9d22f22
Mr. roBoT <innocdavid@gmail.com>
2021-06-04 02:51:04 -0700

    initial commit

---
+++ app/src/main/java/com/example/trafficmapper/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.trafficmapper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //TODO: code for the splash screen.......
+        handler = new Handler();
+        handler.postDelayed(() -> {
+
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 4000);
+
+    }
+}
